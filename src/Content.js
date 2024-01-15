6c7ea0a333bda8e0780eb2819b95e5e8236b5070
SidOmIO <diksiddiq@gmail.com>
2024-01-15 11:22:35 +0800

    Tutorial 5 Click Events

--- src/Content.js
+++ src/Content.js
@@ -6,11 +6,27 @@ const Content = () => {
         const int = Math.floor(Math.random() * 3);
         return names[int];
       }
+
+      const handleClick = () => {
+        console.log('You clicked it');
+      }
+
+      const handleClick2 = (name) => {
+        console.log(`${name} was clicked`);
+      }
+
+      const handleClick3= (e) => {
+        console.log(e.target.innerText);
+      }
+
   return (
     <main>
-        <p>
+        <p onDoubleClick={handleClick}>
             Hello {handleNameChange()}!
         </p>
+        <button onClick={handleClick}>Button 1 (Click It)</button>
+        <button onClick={() => handleClick2('Siddiq')}>Button 2 (Click It)</button>
+        <button onClick={(e) => handleClick3(e)}>Button 3 (Click It)</button>
     </main>
   )
 }
