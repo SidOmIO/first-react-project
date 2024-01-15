445d31a7bc904dcc4987350946f22f303d99b3a2
SidOmIO <diksiddiq@gmail.com>
2024-01-15 11:37:20 +0800

    Tutorial 6 Use State

--- src/Content.js
+++ src/Content.js
@@ -1,34 +1,33 @@
-import React from 'react'
+import { useState } from "react";
 
 const Content = () => {
-    const handleNameChange = () => {
-        const names = ['Bob','Kevin','Dave'];
-        const int = Math.floor(Math.random() * 3);
-        return names[int];
-      }
+  const [name, setName] = useState('Dave');
+  const [count, setCount] = useState(0);
+  const handleNameChange = () => {
+      const names = ['Bob','Kevin','Dave'];
+      const int = Math.floor(Math.random() * 3);
+      setName(names[int]);
+    }
 
-      const handleClick = () => {
-        console.log('You clicked it');
-      }
+    const handleClick = () => {
+      setCount(count + 1);
+      console.log(count);
+    }
 
-      const handleClick2 = (name) => {
-        console.log(`${name} was clicked`);
-      }
+    const handleClick2 = (name) => {
+      console.log(count);
+    }
 
-      const handleClick3= (e) => {
-        console.log(e.target.innerText);
-      }
-
-  return (
-    <main>
-        <p onDoubleClick={handleClick}>
-            Hello {handleNameChange()}!
-        </p>
-        <button onClick={handleClick}>Button 1 (Click It)</button>
-        <button onClick={() => handleClick2('Siddiq')}>Button 2 (Click It)</button>
-        <button onClick={(e) => handleClick3(e)}>Button 3 (Click It)</button>
-    </main>
-  )
+return (
+  <main>
+      <p onDoubleClick={handleClick}>
+          Hello {name}!
+      </p>
+      <button onClick={handleNameChange}>Change Name (Click It)</button>
+      <button onClick={handleClick}>Button 2 (Click It)</button>
+      <button onClick={(e) => handleClick2(e)}>Button 3 (Click It)</button>
+  </main>
+)
 }
 
 export default Content
