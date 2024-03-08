868848f2e3c5a175d4185522ab45cae185dccfca
SidOmIO <diksiddiq@gmail.com>
2024-03-08 16:37:02 +0800

    Tutorial 21 - Context API & useContext Hook

--- src/EditPost.js
+++ src/EditPost.js
@@ -1,11 +1,32 @@
-import { useEffect } from 'react'
+import { useState, useEffect, useContext } from 'react'
 import { useParams, Link } from 'react-router-dom'
+import DataContext from './context/DataContext';
+import { format } from 'date-fns';
+import api from './api/posts';
+import { useNavigate } from 'react-router-dom';
 
-const EditPost = ({
-  posts, handleEdit, editBody, setEditBody, editTitle, setEditTitle
-}) => {
+const EditPost = () => {
+  const { posts, setPosts } = useContext(DataContext);
+  const [editTitle, setEditTitle] = useState('');
+  const [editBody, setEditBody] = useState('');
+  const navigate = useNavigate();
+
   const { id } = useParams();
   const post = posts.find(post => (post.id).toString() === id);
+
+  const handleEdit = async (id) => {
+    const datetime = format(new Date(), 'MMM dd, yyyy pp');
+    const updatedPost = { id, title: editTitle, datetime, body: editBody}
+    try {
+      const response = await api.put(`/posts/${id}`, updatedPost);
+      setPosts(posts.map(post => post.id === id ? {...response.data} : post))
+      setEditTitle('');
+      setEditBody('');
+      navigate('/');
+    } catch (err) {
+      console.log(`Error: ${err.message}`);
+    }
+  }
 
   useEffect(() => {
     if(post) {
